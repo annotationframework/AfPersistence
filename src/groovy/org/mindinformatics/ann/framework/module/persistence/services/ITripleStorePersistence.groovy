fb6ee5b3933fdce2bce187f7363e9dda357b2e0c
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2013-08-09 15:16:47 -0400

    In memory triple store service for testing

---
+++ src/groovy/org/mindinformatics/ann/framework/module/persistence/services/ITripleStorePersistence.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2013 Massachusetts General Hospital
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.mindinformatics.ann.framework.module.persistence.services
+
+import org.openrdf.model.Statement
+
+/**
+ * @author Paolo Ciccarese <paolo.ciccarese@gmail.com>
+ */
+interface ITripleStorePersistence {
+	
+	/**
+	 * Storage of the normalized triples encoding the annotation..
+	 * @param username		Username of the user performing the request
+	 * @param URL			Address of the annotated resource
+	 * @param statement		List of statements encoding the annotations
+	 * @return The URI of the graph created to store the annotation
+	 */
+	public String store(String username, String URL, List<Statement> statements);
+	
+	/**
+	 * Retrieval of annotation for a given resource.
+	 * @param username		Username of the user performing the request
+	 * @param URL			Address of the resource of interest
+	 * @return The collection of annotation statements
+	 */
+	public List<Statement> retrieve(String username, String URL);
+	
+	/**
+	 * Retrieval of annotation public annotation for a given resource.
+	 * @param URL			Address of the resource of interest
+	 * @return The collection of annotation statements
+	 */
+	public List<Statement> retrieve(String URL);
+}
---
+++ src/groovy/org/mindinformatics/ann/framework/module/persistence/services/InMemoryTripleStorePersistenceImpl.groovy
@@ -0,0 +1,102 @@
+package org.mindinformatics.ann.framework.module.persistence.services
+
+import org.ontoware.rdf2go.Reasoning
+import org.ontoware.rdf2go.model.Model
+import org.openrdf.model.Literal
+import org.openrdf.model.Statement
+import org.openrdf.model.ValueFactory
+import org.openrdf.query.QueryLanguage
+import org.openrdf.query.TupleQuery
+import org.openrdf.query.TupleQueryResult
+import org.openrdf.rdf2go.RepositoryModelFactory
+import org.openrdf.repository.Repository
+import org.openrdf.repository.RepositoryConnection
+import org.openrdf.repository.sail.SailRepository
+import org.openrdf.sail.memory.MemoryStore
+
+class InMemoryTripleStorePersistenceImpl implements ITripleStorePersistence {
+	
+	Repository repository;
+	Model model;
+	
+	public InMemoryTripleStorePersistenceImpl() {
+		RepositoryModelFactory mf = new RepositoryModelFactory();
+		model = mf.createModel(Reasoning.owl);
+
+		try {
+			repository = new SailRepository(new MemoryStore());
+			repository.initialize();
+		} catch (Exception e) {
+			System.out.println("D-----> " + e.getMessage());
+		}
+	}
+	
+	@Override
+	public String store(String username, String URL, List<Statement> statements) {
+		ValueFactory f = repository.getValueFactory();
+		org.openrdf.model.URI context1 = f.createURI("http://example.org/" + System.currentTimeMillis());
+		try {
+			RepositoryConnection con = repository.getConnection();
+			try {
+				String namespace = "http://www.foo.com/bar#";
+				org.openrdf.model.URI mySubject = f.createURI(namespace, "actor1");
+				org.openrdf.model.URI myPredicate = f.createURI(namespace, "hasName");
+				Literal myObject = f.createLiteral("Tom Hanks");
+
+				con.add(mySubject, myPredicate, myObject, context1);
+			} finally {
+				con.close();
+			}
+		} catch (Exception e) {
+			// handle exception
+			System.out.println('Exception.... ' + e.getMessage());
+			e.printStackTrace();
+		}
+		RepositoryConnection con2 = repository.getConnection();
+		System.out.println(con2.isEmpty());
+		return context1;
+	}
+
+	@Override
+	public List<Statement> retrieve(String username, String URL) {
+		System.out.println('querying....');
+		RepositoryConnection con = repository.getConnection();
+		try {
+			String queryString = "SELECT ?s ?p ?o WHERE { GRAPH <http://example.org/context1> { ?s ?p ?o .}}";
+			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+			TupleQueryResult result = tupleQuery.evaluate();
+			System.out.println('querying....');
+			while(result.hasNext()) {
+				System.out.println(result.next());
+			}
+		} catch (Exception e) {
+			System.out.println('querying ex....' + e.getMessage());
+
+			e.printStackTrace();
+		} finally {
+			con.close();
+		}
+	}
+
+	@Override
+	public List<Statement> retrieve(String URL) {
+		System.out.println('querying....');
+		RepositoryConnection con = repository.getConnection();
+		try {
+			//String queryString = "SELECT ?s ?p ?o WHERE { GRAPH <http://example.org/1376074729068> { ?s ?p ?o .}}";
+			String queryString = "SELECT ?s ?p ?o WHERE { ?s ?p ?o .}";
+			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+			TupleQueryResult result = tupleQuery.evaluate();
+			System.out.println('querying....');
+			while(result.hasNext()) {
+				System.out.println(result.next());
+			}
+		} catch (Exception e) {
+			System.out.println('querying ex....' + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			con.close();
+		}
+	}
+
+}
