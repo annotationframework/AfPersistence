f39b84f3900a41d6f416fb135abfbdbaec5dc5f6
jmiranda <jmiranda@pih.org>
2014-02-06 12:46:33 -0500

    #31: Added controller action to trigger refresh of the many-to-many association between annotations and tags.

--- grails-app/controllers/org/mindinformatics/ann/framework/module/persistence/AnnotationController.groovy
+++ grails-app/controllers/org/mindinformatics/ann/framework/module/persistence/AnnotationController.groovy
@@ -1,6 +1,27 @@
 package org.mindinformatics.ann.framework.module.persistence
 
+import grails.converters.JSON
+
 class AnnotationController {
 
     def scaffold = Annotation
+    def annotatorService
+
+    def refreshAnnotations() {
+        log.info  '>> Refresh annotation tags for all annotations'
+        def count = 0
+        def annotations = Annotation.list()
+        annotations.collect().each { annotation ->
+            if(annotation.json) {
+                def jsonObject = JSON.parse(annotation.json)
+                if (jsonObject.tags) {
+                    log.info "Updating tags ${jsonObject.tags} for annotation ${annotation.id}"
+                    annotatorService.updateTags(annotation, jsonObject.tags)
+                    count++
+                }
+            }
+        }
+        flash.message = "Refreshed ${count} annotations"
+        redirect(action:"list")
+    }
 }
