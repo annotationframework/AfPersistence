3df136cf53f97a37a81f59aad8d787c76bef7612
jmiranda <jcm62@columbia.edu>
2014-10-07 17:33:49 -0400

    #37 Attempt to fix dependency resolution issue -- was not finding 'org.semweb4j:rdf2go.impl.base:4.8.2' dependency so I removed it since it doesn't look like it's been necessary since v4.6.2.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,7 +27,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
 		mavenRepo "http://repo.aduna-software.org/maven2/releases/"
 		mavenRepo "http://repo1.maven.org/maven2/"
-		mavenRepo "http://mavenrepo.fzi.de/semweb4j.org/repo/"
+        mavenRepo "http://semweb4j.org/repo/"
 		//mavenRepo "https://repository.jboss.org/nexus/content/repositories/thirdparty-releases"
 		
     }
@@ -40,19 +40,18 @@ grails.project.dependency.resolution = {
 
         //compile 'commons-codec:commons-codec:1.5'
 		compile ("org.apache.marmotta:sesame-tools-rio-jsonld:3.0.0-incubating")
-		compile ("org.semweb4j:rdf2go.impl.sesame:4.8.2")
-		compile ("org.semweb4j:rdf2go.api:4.8.2")
+        compile ("org.semweb4j:rdf2go.impl.sesame:4.8.2")
+        compile ("org.semweb4j:rdf2go.api:4.8.2")
         compile ("com.nimbusds:nimbus-jose-jwt:2.20")
         //compile ("com.googlecode.jsontoken:jsontoken:1.0")
-		
+
 		compile ("org.apache.jena:jena-core:2.11.0") {
 			excludes 'slf4j-api', 'xercesImpl'
 		}
 		compile ("org.apache.jena:jena-arq:2.9.3") 
 		
-        runtime "org.semweb4j:rdf2go.impl.sesame:4.8.2"
-		runtime "org.semweb4j:rdf2go.api:4.8.2"
-		compile "org.semweb4j:rdf2go.impl.base:4.6.2"
+        //runtime "org.semweb4j:rdf2go.impl.sesame:4.8.2"
+		//runtime "org.semweb4j:rdf2go.api:4.8.2"
 		//compile "org.openrdf:openrdf-sesame-onejar-osgi:2.1.2"
 		//compile "org.openrdf.sesame:sesame-query:2.7.2"
 
@@ -68,6 +67,10 @@ grails.project.dependency.resolution = {
 	    compile ":spring-security-openid:2.0-RC2"
         compile ":build-test-data:2.0.9"
 
+        // #21 Keep this here so commons-codec:1.3 does not get included
+        compile (":functional-test:2.0.RC1") {
+            excludes "commons-codec"
+        }
 
     }
 }
